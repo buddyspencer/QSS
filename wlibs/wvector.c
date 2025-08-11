b2fc331fa861386b260bcf4ce0b6058f64efa603
Walter Rosario Molino <walmol@gmail.com>
2025-08-11 12:14:11 +0200

    Update wvector.c
    update comment shift_vector

--- wlibs/wvector.c
+++ wlibs/wvector.c
@@ -25,12 +25,24 @@ void wVectorCopy(float wvector_to[], float wvector_from[],int tot_elem){
     for (int i=0; i<tot_elem; i++) wvector_to[i]=wvector_from[i];
 
 }
-//shift vector byte of MAX_BYTE64 element for bit_maschera position
+
+
+/*shift vector byte of MAX_BYTE64 element for bit_maschera position
+* for example for 2 byte 
+* | 00000001 | 10000011 | 
+*     Byte 1    Byte 0
+*
+* 1° Interaction  bit_maschera = 1  precedente_msb = 1  byte[0] = 00000111  bit_maschera = 1
+* 2° interaction  bit_maschera = 1  precedente_msb = 0  byte[1] = 00000011  bit_maschera = 0
+* final result:
+* | 00000011 | 00000111 |
+*/
+
 void shiftLeft(uint64_t bit_maschera,uint64_t byte[]){
     for (int i=0; i<MAX_BYTE64; i++){
-        uint64_t precedente_msb = byte[i]>>63;
-        byte[i] = (byte[i]<<1) | bit_maschera;
-        bit_maschera = precedente_msb;
+        uint64_t precedente_msb = byte[i]>>63;     //take the bit MSB
+        byte[i] = (byte[i]<<1) | bit_maschera;     // shitft left 1 position and insert the bit_maschera
+        bit_maschera = precedente_msb;             //bit_maschera have bit MSB for next Byte
     }   
 
 }
